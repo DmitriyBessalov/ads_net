df277e8bf9f269824557587ec824be4a9f47af23
dmitriy <dmitriy@bessalov.ru>
2019-03-31 15:08:52 +0300

    Revert "Initial commit"
    This reverts commit f9de1f69

--- stat.corton.io/compress_corton.js.php
+++
@@ -1,9 +0,0 @@
-<?php
-// Сжимать файл corton.js
-$data = implode("", file("/var/www/www-root/data/www/api.corton.io/js/corton.js"));
-$gzdata = gzencode($data, 9);
-$fp = fopen("/var/www/www-root/data/www/api.corton.io/js/corton.js.gz", "w");
-fwrite($fp, $gzdata);
-fclose($fp);
-unset($gzdata);
-echo '1';
