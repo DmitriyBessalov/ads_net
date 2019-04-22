47f88033a55d6e16d677c7d90dbae41486049df2
Mitiplay <surov_job@mail.ru>
2019-04-22 15:40:45 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -305,7 +305,7 @@ class FinController
                     document.getElementById(result_id).innerHTML = response;
                 },
                 error: function(response) {
-                    document.getElementById(result_id).innerHTML = "<p>Сообщение отправлено, скоро с Вами свяжется менежер</p>";
+                    document.getElementById(result_id).innerHTML = "<p style="color:#60bf52; font-size:16px;line-height:20px;font-family:\'Myriadpro Regular\';">Сообщение отправлено, скоро с Вами свяжется менежер</p>";
                 }
             });
             $(\':input\',\'#formMain\') .prop(\'disabled\',true)
