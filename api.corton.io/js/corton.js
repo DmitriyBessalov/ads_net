79ac17a0a3d48e1487150ee6a539e2418d36a401
Mitiplay <surov_job@mail.ru>
2019-04-26 17:35:08 +0300

    Update corton.js

--- api.corton.io/js/corton.js
+++ api.corton.io/js/corton.js
@@ -71,6 +71,26 @@ function corton_promo() {
         if (promo_form) {
             h3=promo_form.scrollHeight;
         };
+		
+		//PopUp партнерской иконки
+		$(function() {
+
+	$('[data-popup-open]').on('click', function(e) {
+		var targeted_popup_class = jQuery(this).attr('data-popup-open');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeIn(350);
+
+		e.preventDefault();
+	});
+
+	$('[data-popup-close]').on('click', function(e) {
+		var targeted_popup_class = jQuery(this).attr('data-popup-close');
+		$('[data-popup="' + targeted_popup_class + '"]').fadeOut(350);
+
+		e.preventDefault();
+	});
+});
+		
+		
 
         //Клик по ссылке в промо статье
         var a = document.querySelectorAll('div#corton-promo a');
