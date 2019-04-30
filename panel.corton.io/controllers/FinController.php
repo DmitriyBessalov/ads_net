c830187a520261d556290f449bdb0c46144ebb27
Mitiplay <surov_job@mail.ru>
2019-04-30 14:40:45 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -413,8 +413,12 @@ class FinController
 		</div>  
         <div style="border-width: 0 0 0 1px; border-style: solid; color:#E0E1E5; padding: 20px; margin-left: 40px;"></div>		
 		<div>   
-		   <div class="text-block-104">Колличество обработанных уников</div>
+		   <div class="text-block-104">Колличество визитов</div>
            <div class="text-block-105">70 140</div>
+		</div>
+		<div>   
+		   <div class="text-block-104">Процент показа виджетов</div>
+           <div class="text-block-105">36%</div>
 		</div>
 		</div>
     </div>
