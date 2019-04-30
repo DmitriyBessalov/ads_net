45e24b7a6ed97f9619fdd30e3b76082ebc4a1286
Mitiplay <surov_job@mail.ru>
2019-04-30 18:15:27 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -443,11 +443,20 @@ class FinController
         <div class="text-block-103">ТОП 5 площадок и ключей</div>
         <div class="text-block-104">Популярные площадки и ключи</div>
 		<div style="padding-bottom:14px;"></div>
-        <span class="topwords"><span style="color:#768093;">1.</span> Артроз</span><span style="right:40px;" class="topwords"><span style="color:#768093;">1.</span> Артроз</span><br>
-		<span class="topwords"><span style="color:#768093;">2.</span> Давление</span><br>
-		<span class="topwords"><span style="color:#768093;">3.</span> Кардио</span><br>
-		<span class="topwords"><span style="color:#768093;">4.</span> Инсулин</span><br>
-		<span class="topwords"><span style="color:#768093;">5.</span> Диабет</span>
+		<div>
+           <span class="topwords"><span style="color:#768093;">1.</span> Артроз</span><br>
+		   <span class="topwords"><span style="color:#768093;">2.</span> Давление</span><br>
+		   <span class="topwords"><span style="color:#768093;">3.</span> Кардио</span><br>
+		   <span class="topwords"><span style="color:#768093;">4.</span> Инсулин</span><br>
+		   <span class="topwords"><span style="color:#768093;">5.</span> Диабет</span>
+		</div>
+		<div>
+           <span class="topwords"><span style="color:#768093;">1.</span> Артроз</span><br>
+		   <span class="topwords"><span style="color:#768093;">2.</span> Давление</span><br>
+		   <span class="topwords"><span style="color:#768093;">3.</span> Кардио</span><br>
+		   <span class="topwords"><span style="color:#768093;">4.</span> Инсулин</span><br>
+		   <span class="topwords"><span style="color:#768093;">5.</span> Диабет</span>
+		</div>
     </div>
 </div>
 
