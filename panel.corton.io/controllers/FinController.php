1e1958a97769062c628d8a345eaed9f9963f6bda
Mitiplay <surov_job@mail.ru>
2019-05-04 16:39:25 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -386,10 +386,7 @@ class FinController
         echo '<script>$(".text-block-balans").html("'.$balans.' р.");</script>';
         return true;
     }
-	    
-		include PANELDIR . '/views/layouts/footer.php';
-		echo '<script>$(".sub-menutwo").html("Вывод балансов");</script>';
-    }
+	    	
 
     public static function actionAdmin()
     {
