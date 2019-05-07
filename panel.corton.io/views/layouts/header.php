6265ea7817b754a19dd242810a2236426f07490f
Mitiplay <surov_job@mail.ru>
2019-05-07 11:05:43 +0300

    Обновление графиков

--- panel.corton.io/views/layouts/header.php
+++ panel.corton.io/views/layouts/header.php
@@ -12,6 +12,8 @@
   <script type="text/javascript">WebFont.load({google: {families: ["Open Sans:300,300italic,400,400italic,600,600italic,700,700italic,800,800italic","Inconsolata:400,700","Roboto:300,300italic,regular,italic,500,500italic,700,700italic:cyrillic,latin"]  }});</script>
   <script type="text/javascript">!function(o,c){var n=c.documentElement,t=" w-mod-";n.className+=t+"js",("ontouchstart" in o||o.DocumentTouch&&c instanceof DocumentTouch)&&(n.className+=t+"touch")}(window,document);</script>
   <script type="text/javascript" src="/js/tcal.js"></script>
+  <script src="https://panel.corton.io/js/Chart.min.js"></script>
+  <script src="https://panel.corton.io/js/utils.js"></script>
   <meta name="robots" content="none">
 </head>
 <body class="body">
