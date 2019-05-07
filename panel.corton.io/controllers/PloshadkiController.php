ae280e7277adf4f219892949137893b71ca690b1
Mitiplay <surov_job@mail.ru>
2019-05-07 15:23:19 +0300

    Update PloshadkiController.php

--- panel.corton.io/controllers/PloshadkiController.php
+++ panel.corton.io/controllers/PloshadkiController.php
@@ -141,6 +141,7 @@ var dataset_01 = {
 	pointBorderColor: \"rgba(17,109,214,1)\",
 	borderWidth: \"2\",
 	pointRadius: 2,
+	padding: 4,
     data: [30100, 32000, 33300, 29005, 31405, 32604, 19045]
 };
 
