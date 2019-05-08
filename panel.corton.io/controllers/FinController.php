2627903da43a58ed651350ec17759d2f3c7156b9
Mitiplay <surov_job@mail.ru>
2019-05-08 12:14:04 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -449,7 +449,7 @@ class FinController
         <div class="text-block-103">Внешние метрики</div>
 		<div style="display: flex;">
 		<div style="width:33%;">
-           <div class="text-block-104">Колличество визитов</div>
+           <div class="text-block-104">Просмотров страниц</div>
            <div style="font-size: 46px;" class="text-block-105">(128 315)</div>
 		</div>
         <div style="border-width: 0 0 0 1px; border-style: solid; color:#E0E1E5; padding: 20px; margin-left: 40px;"></div>		
