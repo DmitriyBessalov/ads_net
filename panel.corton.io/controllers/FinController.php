e3a1dc9857446e37da23dcb3b67fc8e23697ab68
Mitiplay <surov_job@mail.ru>
2019-05-08 11:14:42 +0300

    Update FinController.php

--- panel.corton.io/controllers/FinController.php
+++ panel.corton.io/controllers/FinController.php
@@ -640,11 +640,11 @@ var options = {
         yAxes: [{ display: false }]
     },
 	borderWidth: {
-   top: 1,
-   right: 0,
-   bottom: 0,
-   left: 0
-}	
+       top: 2,
+       right: 2,
+       bottom: 0,
+       left: 0
+    }	
 };
 
 var ctx = document.getElementById("a").getContext("2d");
