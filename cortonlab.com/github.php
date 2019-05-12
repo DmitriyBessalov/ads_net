073c3f9cc80f3873bf2393e1b3e589479b53a5fe
dmitriy <dmitriy@bessalov.ru>
2019-05-12 12:44:55 +0300

    Обновление

--- cortonlab.com/github.php
+++ cortonlab.com/github.php
@@ -1,3 +1,3 @@
 <?php
-exec('cd /var/www/www-root/data/www && git reset --hard origin/master');
-echo '{"status":"ok"}';
+echo exec('cd /var/www/www-root/data/www && git reset --hard origin/master');
+//echo '{"status":"ok"}';
