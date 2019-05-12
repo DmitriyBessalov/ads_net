8aa227e792420b0fe3d2f294210ff91187d6797f
dmitriy <dmitriy@bessalov.ru>
2019-05-12 12:56:51 +0300

    Обновление

--- cortonlab.com/github.php
+++ cortonlab.com/github.php
@@ -2,4 +2,3 @@
 echo exec('cd /var/www/www-root/data/www && git reset --hard origin/master ');
 
 //exec('cd /var/www/www-root/data/www && git reset --hard HEAD && git pull git@github.com:DmitriyBessalov/corton.git');
-//echo '{"status":"ok"}';
