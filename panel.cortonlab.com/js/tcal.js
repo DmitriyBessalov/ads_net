d3bb407f8b3459cf8af5e4f2d1cdf00f4ae757df
Mitiplay <surov_job@mail.ru>
2019-05-13 10:54:45 +0300

    Update tcal.js

--- panel.cortonlab.com/js/tcal.js
+++ panel.cortonlab.com/js/tcal.js
@@ -7,7 +7,7 @@ var A_TCALCONF = {
 	'cssprefix'  : 'tcal',
 	'months'     : ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'],
 	'weekdays'   : ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'],
-	'longwdays'  : ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'],
+	'longwdays'  : ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'],
 	'yearscroll' : true, // show year scroller
 	'weekstart'  : 0, // first day of week: 0-Su or 1-Mo
 	'prevyear'   : 'Предыдущий год',
