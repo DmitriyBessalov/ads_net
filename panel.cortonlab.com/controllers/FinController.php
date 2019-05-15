0f7243942545e4183f663c8f205a15e866370317
Mitiplay <surov_job@mail.ru>
2019-05-15 17:50:55 +0300

    Update FinController.php

--- panel.cortonlab.com/controllers/FinController.php
+++ panel.cortonlab.com/controllers/FinController.php
@@ -14,7 +14,7 @@ class FinController
         $mySQLdateend = date('Y-m-d', strtotime($dateend));
 
         echo '
-//info block	
+/* info block */	
 <!-- <div class="message-box">
     <p><span style="font-weight:400; color: #fff;">Обновление от 16.05.2019.</span> Теперь вывод балансов можно запрашивать в личном кабинете. Для этого нажмите на иконку пользователя и выберите пункт "Вывод балансов".</p>
     <span class="close-button">Понял, больше не показывать.</span>
