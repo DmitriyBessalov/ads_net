6a2d6609fdddf80481baec49ae9141527174a487
Mitiplay <surov_job@mail.ru>
2019-06-01 19:54:35 +0300

    Обновление меню

--- panel.cortonlab.com/views/layouts/article_header.php
+++ panel.cortonlab.com/views/layouts/article_header.php
@@ -9,6 +9,14 @@ echo '
    <a href="/article-edit-target?id='.$_GET['id'].'" class="btnarticlegr">Таргетинги</a>
    <a href="/article-stat-url?id='.$_GET['id'].'" class="btnarticlegr">Анализ ссылок</a>
    <a href="/article-edit-form?id='.$_GET['id'].'" class="btnarticlegr">Форма заказа</a>
-   <a href="/article-stat?id='.$_GET['id'].'" class="btnarticle">Расширенная статистика</a><button class="btn btn-danger dropdown-toggle" data-toggle="dropdown"> <i class="icon-caret-down" style="font-size: 0.65em;"></i> </button>
+   <a href="/article-stat?id='.$_GET['id'].'" class="btnarticle">Расширенная статистика</a>
+   <div class="dropdown">
+      <button onclick="myFunction()" class="dropbtn"><i class="fa fa-caret-down"></i></button>
+   <div id="myDropdown" class="dropdown-content">
+     <a href="#home">Home</a>
+     <a href="#about">About</a>
+     <a href="#contact">Contact</a>
+   </div>
+</div>
 </div>
 ';
