c9ba42558d9a0bf0d8d8030dd979cb5fc46094bf
Mitiplay <surov_job@mail.ru>
2019-06-01 20:23:33 +0300

    Update article_header.php

--- panel.cortonlab.com/views/layouts/article_header.php
+++ panel.cortonlab.com/views/layouts/article_header.php
@@ -4,11 +4,9 @@ include PANELDIR.'/views/layouts/header.php';
 echo '
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="btncontrolarticle">
-   <a href="/article-a/b?id='.$_GET['id'].'" class="btnarticlegr">A/B анализ</a>
    <a href="/article-edit-content?id='.$_GET['id'].'" class="btnarticlegr">Контент статей</a>
    <a href="/article-edit-anons?id='.$_GET['id'].'" class="btnarticlegr">Редактировать анонсы</a>
    <a href="/article-edit-target?id='.$_GET['id'].'" class="btnarticlegr">Таргетинги</a>
-   <a href="/article-stat-url?id='.$_GET['id'].'" class="btnarticlegr">Анализ ссылок</a>
    <a href="/article-edit-form?id='.$_GET['id'].'" class="btnarticlegr">Форма заказа</a>
    <a href="/article-stat?id='.$_GET['id'].'" class="btnarticle" style="border-radius: 4px 0 0 4px;">Cтатистика</a>
    <div class="dropdown">
