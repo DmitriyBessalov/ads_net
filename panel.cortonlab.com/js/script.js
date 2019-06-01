7e26e804a7a68bcdb9f95efc7aa1746af89547f9
Mitiplay <surov_job@mail.ru>
2019-06-01 20:12:32 +0300

    Update script.js

--- panel.cortonlab.com/js/script.js
+++ panel.cortonlab.com/js/script.js
@@ -990,3 +990,22 @@ $(function(){
         $('.message-box').fadeOut(200);
     });
 })
+
+/* Выпадающая кнопка для админки */
+function myFunction() {
+    document.getElementById("myDropdown").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
