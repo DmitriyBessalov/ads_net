182eafeffdae95e49a7c83108b6603c04b410022
dmitriy <dmitriy@bessalov.ru>
2019-06-04 17:53:07 +0300

    КП

--- api.cortonlab.com/js/promo.php
+++ api.cortonlab.com/js/promo.php
@@ -7,8 +7,10 @@ $db = new PDO("mysql:host=185.75.90.54;dbname=corton", 'corton', 'W1w5J7e6', arr
 $sql="SELECT `promo_id` FROM `anons` WHERE `id`='".addslashes($_GET['anons_id'])."'";
 $promo_id = $db->query($sql)->fetch(PDO::FETCH_COLUMN);
 
+$items=Array($promo_id);
+
 //Выдача промо статьи
-$sql="SELECT `id`,`title`,`text`,`form_title`,`active`,`form_text`,`form_button` FROM `promo` WHERE `id`='".$promo_id."'";
+$sql="SELECT `id`,`title`,`text`,`form_title`,`active`,`form_text`,`form_button` FROM `promo` WHERE `id`='".$items[array_rand($items)]."'";
 $result = $db->query($sql)->fetch(PDO::FETCH_ASSOC);
 
 if ($result['active'] or $_SERVER['HTTP_ORIGIN']=='https://demo.cortonlab.com'){
