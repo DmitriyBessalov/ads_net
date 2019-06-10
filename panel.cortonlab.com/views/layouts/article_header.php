9d4099dac096a044cd06dfbd9fda7f2cc8f3d915
Mitiplay <surov_job@mail.ru>
2019-06-10 19:43:16 +0300

    Update article_header.php

--- panel.cortonlab.com/views/layouts/article_header.php
+++ panel.cortonlab.com/views/layouts/article_header.php
@@ -9,7 +9,7 @@ echo '
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 <div class="btncontrolarticle">
    <a href="/article-edit-content?id='.$result['main_promo_id'].'" class="btnarticlegr">Контент статьи</a>
-   <a href="/article-edit-anons?id='.$result['main_promo_id'].'" class="btnarticlegr">Анонсы</a>
+   <a href="/article-edit-anons?id='.$result['main_promo_id'].'" class="btnarticlegr">Анонсы статьи</a>
    <a href="/article-edit-target?id='.$result['main_promo_id'].'" class="btnarticlegr">Таргетинги</a>
    <a href="/article-edit-form?id='.$result['main_promo_id'].'" class="btnarticlegr">Форма заказа</a>
    <a href="/article-stat?id='.$result['main_promo_id'].'" class="btnarticle" style="border-radius: 4px 0 0 4px;">Cтатистика</a>
