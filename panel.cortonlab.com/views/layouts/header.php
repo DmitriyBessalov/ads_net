f0d93cd874b997b2738f5f79fc1fc596b1d4302e
Mitiplay <surov_job@mail.ru>
2019-06-21 11:34:38 +0300

    вывод

--- panel.cortonlab.com/views/layouts/header.php
+++ panel.cortonlab.com/views/layouts/header.php
@@ -22,7 +22,7 @@
 	  <img src="/images/cortonlab.png" alt="" class="image">
       <? if ($GLOBALS['role']=='platform'): ?>
           <a href="/finance" class="link-block w-inline-block"><img src="/images/ic-fin.png" class="image-6"><div class="text-block-82-copy">Статистика</div></a>
-		  <a href="/balance" class="link-block w-inline-block"><img src="/images/ic-fin.png" class="image-6"><div class="text-block-82-copy">Вывод средств</div></a>
+		  <a href="/balance" class="link-block w-inline-block"><img src="/images/ic-balance.png" class="image-6"><div class="text-block-82-copy">Вывод средств</div></a>
       <? endif; ?>
       <? if ($GLOBALS['role']=='advertiser'): ?>
           <a href="/articles?active=all" class="link-block w-inline-block"><img src="/images/ic-content.png" class="image-6"><div class="text-block-82">Статьи</div></a>
