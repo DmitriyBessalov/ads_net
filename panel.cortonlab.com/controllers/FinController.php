4ad2d3e49b6b39a3650c2d77fc33c50a89ef420a
Mitiplay <surov_job@mail.ru>
2019-06-21 12:07:15 +0300

    меню

--- panel.cortonlab.com/controllers/FinController.php
+++ panel.cortonlab.com/controllers/FinController.php
@@ -426,7 +426,7 @@ class FinController
 									 </div>
 								    <div class="btnbalans" id="button_vivod">Запросить вывод средств</div>
 								    <p id="status_vivod"></p>
-									<p class="textinfobal">Минимальная сумма к выводу 5000 рублей. Согласно <a href="https://cortonlab.com/terms-of-use" target="_blank">правилам</a> средства могут быть перечислены в течение 9 рабочих дней после запроса на вывод.</p>
+									<p class="textinfobal" style="max-width: 600px;">Минимальная сумма к выводу 5000 рублей. Согласно <a href="https://cortonlab.com/terms-of-use" target="_blank">правилам</a> средства могут быть перечислены в течение 9 рабочих дней после запроса на вывод.</p>
                                 </div>
         <script>
         function AjaxFormRequest(result_id,formMain,url) {
