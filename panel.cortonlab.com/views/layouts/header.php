e4715f3f9f94b9d902e14079ffd0ca599d3184ae
Mitiplay <surov_job@mail.ru>
2019-06-21 12:00:49 +0300

    Update header.php

--- panel.cortonlab.com/views/layouts/header.php
+++ panel.cortonlab.com/views/layouts/header.php
@@ -23,7 +23,7 @@
       <? if ($GLOBALS['role']=='platform'): ?>
           <a href="/finance" class="link-block w-inline-block"><img src="/images/ic-fin.png" class="image-6"><div class="text-block-82-copy">Статистика</div></a>
 		  <a href="/balance" class="link-block w-inline-block"><img src="/images/ic-balance.png" class="image-6"><div class="text-block-82-copy">Вывод средств</div></a>
-		  <a href="/balance" class="link-block w-inline-block"><img src="/images/ic-balance.png" class="image-6"><div class="text-block-82-copy">Настройки</div></a>
+		  <a href="/balance" class="link-block w-inline-block"><img src="/images/ic-setting.png" class="image-6"><div class="text-block-82-copy">Настройки</div></a>
       <? endif; ?>
       <? if ($GLOBALS['role']=='advertiser'): ?>
           <a href="/articles?active=all" class="link-block w-inline-block"><img src="/images/ic-content.png" class="image-6"><div class="text-block-82">Статьи</div></a>
