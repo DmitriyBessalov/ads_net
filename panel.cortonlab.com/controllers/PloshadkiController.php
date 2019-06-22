aa6f477c03a093e8d80dbd3b5b1cce3d3fb8a92d
Mitiplay <surov_job@mail.ru>
2019-06-22 20:05:28 +0300

    Update PloshadkiController.php

--- panel.cortonlab.com/controllers/PloshadkiController.php
+++ panel.cortonlab.com/controllers/PloshadkiController.php
@@ -57,7 +57,6 @@
                 <td>Площадка</td>
                 <td>Виджеты</td>
                 <td>Визиты</td>
-                <td>Уники</td>
                 <td>Клики</td>
                 <td><div class="tooltipinfo1">Просмотры<span class="tooltiptext1">Целевые/оплаченные просмотры промо-статей и процент от кликов</span></div></td>
                 <td style="width: 111px;"><div class="tooltipinfo1">График<span class="tooltiptext1">График по просмотрам за последние 7 дней</span></div></td>
@@ -122,7 +121,6 @@
                         <div class=\"slider-mini\""; if (!$i['slider_aktiv']) {echo " style='opacity: 0.3;'";}; echo"><span class=\"tooltiptext4\">Slider</span></div>
                        </div>
                       </td>
-                      <td>0</td>
                       <td>0</td>
                       <td>".$platform['click']."</td>
                       <td class=\"greentext\" style=\"min-width: 140px;\">".$platform['prosmotr']." (".$protsent_prochteniy."%)</td>
