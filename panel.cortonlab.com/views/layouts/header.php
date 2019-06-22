2bfe9abdf5ec9224f93b8667736e7164b7ba7c49
Mitiplay <surov_job@mail.ru>
2019-06-22 14:28:54 +0300

    Update header.php

--- panel.cortonlab.com/views/layouts/header.php
+++ panel.cortonlab.com/views/layouts/header.php
@@ -42,15 +42,15 @@
 			<div class="titlehr" style="margin-top:10px;">Управление</div>
 	        <a href="/platforms?status=1" class="link-block w-inline-block"><img src="/images/ic-platform.png" class="image-6"><div class="text-block-82-copy">Площадки</div></a>
             <a href="/articles?active=1" class="link-block w-inline-block"><img src="/images/ic-content.png" class="image-6"><div class="text-block-82">Статьи</div></a>
-			<a href="/users" class="link-block w-inline-block"><img src="/images/ic-user.png" class="image-6"><div class="text-block-82">Пользователи</div></a>
+			
 			<div class="titlehr" style="margin-top:10px;">Системные</div>
 			<a href="/notifications" class="link-block w-inline-block"><img src="/images/ic-notice.png" class="image-6"><div class="text-block-82">Уведомления</div>
                 <?
                 $sql = "SELECT COUNT(*) FROM `notifications` WHERE `status`='0'";
                 $notification = $GLOBALS['db']->query($sql)->fetch(PDO::FETCH_COLUMN);
                 if ($notification) echo '<div class="circlnotice" >'.$notification.'</div></a>';
-                ?>
-            
+                ?>    
+			<a href="/users" class="link-block w-inline-block"><img src="/images/ic-user.png" class="image-6"><div class="text-block-82">Пользователи</div></a>	
       <? endif; ?>
             <!--a href="/tickets" class="link-block w-inline-block"><img src="/images/ic-ticket.png" class="image-6"><div class="text-block-82">Тикеты</div></a-->
   </div>
