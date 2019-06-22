09d5d22ddd8f0b4c989e4f49ba983b33cfac6b8e
Mitiplay <surov_job@mail.ru>
2019-06-22 16:40:05 +0300

    Update PloshadkiController.php

--- panel.cortonlab.com/controllers/PloshadkiController.php
+++ panel.cortonlab.com/controllers/PloshadkiController.php
@@ -540,7 +540,6 @@ var myLineChart = new Chart(ctx, {
                 </div>
               </div>';}
                 echo'
-			  <div style="border-top: 1px solid #E0E1E5 !important; width: 1337px; margin-bottom: 60px;"></div>
 			  <input type="submit" value="Сохранить настройки" class="submit-button-6 w-button2" '.$disabled.'>
 			  
             </form>
