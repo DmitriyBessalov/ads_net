3a737a678b5220298d84026270a834a1ef5521c8
Mitiplay <surov_job@mail.ru>
2019-06-24 12:03:56 +0300

    Update FinController.php

--- panel.cortonlab.com/controllers/FinController.php
+++ panel.cortonlab.com/controllers/FinController.php
@@ -588,8 +588,8 @@ class FinController
         <div class="text-block-103">Доход площадок</div>
         <div class="text-block-104">Объём заработанных средств площадками</div>
         <div class="text-block-105">'.$result['dohod'].'</div>
-	<div id="containergr" style="width:300px; height:102px;">
-	<canvas id="c" width="305" height="102"></canvas>
+	<div id="containergr" style="width:305px; height:102px;">
+	<canvas id="c" width="310" height="102"></canvas>
 	</div>
     </div>
     <div class="div-block-94">
