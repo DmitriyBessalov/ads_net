834b709655509f1f289f659fb80198642502d2f1
Mitiplay <surov_job@mail.ru>
2019-06-24 10:44:40 +0300

    Update FinController.php

--- panel.cortonlab.com/controllers/FinController.php
+++ panel.cortonlab.com/controllers/FinController.php
@@ -20,6 +20,7 @@ class FinController
 </div> -->	
 
 <div class="table-box">
+    <div class="div-block-102-table">
     <div class="table w-embed">
         <table>
             <thead>
@@ -76,7 +77,7 @@ class FinController
 
             $sql = "SELECT SUM(`r_balans`) as `r_balans`,SUM(`e_balans`) as `e_balans`, SUM(`s_balans`) as `s_balans`, SUM(`r`) as `r`, SUM(`e`) as `e`, SUM(`s`) as `s`, SUM(`r_show_anons`) as 'r_show_anons', SUM(`e_show_anons`) as 'e_show_anons', SUM(`s_show_anons`) as 's_show_anons', SUM(`r_promo_load`) as 'r_promo_load', SUM(`e_promo_load`) as 'e_promo_load', SUM(`s_promo_load`) as 's_promo_load' FROM `balans_ploshadki` WHERE `ploshadka_id` in ('" . $strplatform . "') AND `date`>='" . $mySQLdatebegin . "' AND `date`<='" . $mySQLdateend . "'";
             $balansperiod = $GLOBALS['dbstat']->query($sql)->fetch(PDO::FETCH_ASSOC);
-
+е
             if ((strtotime($datebegin) <= strtotime(date('d.m.Y'))) AND (strtotime($dateend) >= strtotime(date('d.m.Y')))) {
                 $today = true;
                 $redis = new Redis();
@@ -279,6 +280,7 @@ class FinController
         echo'
         </table>
                 </div>
+		</div>		
                     <div class="table-right">
 				 <form id="right-form" name="email-form" class="form-333">
 			         <a href="/finance#openaddsite" class="button-add-site w-button">Добавить площадку</a>
