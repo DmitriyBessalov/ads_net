31e21cfbbfce1640d27db9c3c0d7fce41e793209
Mitiplay <surov_job@mail.ru>
2019-06-24 15:26:47 +0300

    Update ArticleController.php

--- panel.cortonlab.com/controllers/ArticleController.php
+++ panel.cortonlab.com/controllers/ArticleController.php
@@ -321,10 +321,7 @@ class ArticleController
 
                 echo '
              <tr>';
-                if ($ch2 != -1) {
-                    $sql = "SELECT `user_id`,`img_290x180`,`title`,`active` FROM `anons` WHERE `id`='" . $anons . "'";
-                    $img = $GLOBALS['db']->query($sql)->fetch(PDO::FETCH_ASSOC);
-                    echo '
+                 '
                           <td><a class="screenshot" style="text-decoration:none;" rel="https://api.cortonlab.com/img/' . $img['user_id'] . '/a/' . $img['img_290x180'] . '" ><img style="max-width: 70px !important; border-radius: 2px;" src="https://api.cortonlab.com/img/' . $img['user_id'] . '/a/' . $img['img_290x180'] . '"></a></td>';
                     echo '<td style="width: 180px !important;"><div class=titleform>' . $img['title'] . '</div></td>';
                 } else {
