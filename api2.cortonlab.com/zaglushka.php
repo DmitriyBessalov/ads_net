e166a608d9cb66d1c8ab0b4518067cb07a2355cf
dmitriy <dmitriy@bessalov.ru>
2019-06-27 14:01:48 +0300

    add api2.cortonlab.com

---
+++ api2.cortonlab.com/zaglushka.php
@@ -0,0 +1,17 @@
+<?php
+$_GET = array_map('addslashes', $_GET);
+require_once('/var/www/www-root/data/www/panel.cortonlab.com/config/db.php');$data= $GLOBALS['db']->query("SELECT `code` FROM `zag_".$_GET['tizer']."` WHERE `id`='".$_GET['id']."'")->fetch(PDO::FETCH_COLUMN);
+echo
+'<!DOCTYPE html>
+<html>
+<body style="margin: 0px;">
+'.$data.'
+<script>
+    window.onload = function() {
+        var url = location.protocol+"//'.$_GET['host'].'";
+        var str=\'{"corton_tizer":"'.$_GET['tizer'].'","height":"\'+document.body.scrollHeight+\'"}\';
+        parent.postMessage(str,url);
+    };
+</script>
+</body>
+</html>';
