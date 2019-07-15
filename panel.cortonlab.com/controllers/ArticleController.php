5d1cda8cdfb5bdabf20c3d5fa7c926befb460159
Mitiplay <surov_job@mail.ru>
2019-07-15 15:53:37 +0300

    Update ArticleController.php

--- panel.cortonlab.com/controllers/ArticleController.php
+++ panel.cortonlab.com/controllers/ArticleController.php
@@ -139,6 +139,9 @@ class ArticleController
 	                <div style="background-image: url(https://api.cortonlab.com/img/promo/121/76c7fa6f.jpeg); background-position-x: center; background-size: cover; height:100%; border-radius: 8px; padding:20px;">
 	                <div style="height:30px; margin-top: -8px;">
 					<div class="menuarticle">
+					<input type="checkbox" ';
+                    if ($_GET['active']) echo 'checked="checked "';
+                    echo ' class="flipswitch all"/>
 	                     <a class="main-item" href="javascript:void(0);" tabindex="1" style="font-size: 34px; line-height: 12px; vertical-align: super; text-decoration: none; color: #768093;">...</a>
 			                 <ul class="sub-menu-content">
                                  <a href="article-edit-content?id=' . $i['main_promo_id'] . '">Отредактировать</a><br>
