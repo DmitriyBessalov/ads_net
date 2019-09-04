5788c4d93025c80f3050ccd1bf608e1a08d8fcb6
dmitriy <dmitriy@bessalov.ru>
2019-09-04 16:14:27 +0300

    -

--- stat.cortonlab.com/widget_show.php
+++ stat.cortonlab.com/widget_show.php
@@ -1,23 +1,36 @@
 <?php
 header('Access-Control-Allow-Origin: *');
+
+require_once('/var/www/www-root/data/www/panel.cortonlab.com/config/db.php');
+
+$_SERVER['HTTP_ORIGIN']='http://okardio.com/dfhgj';
+
+$domen=parse_url ( $_SERVER['HTTP_ORIGIN'], PHP_URL_HOST );
+$sql= "SELECT `id` FROM `ploshadki` WHERE `domen`='".$domen."'";
+
+$ploshadka_id = $GLOBALS['db']->query($sql)->fetch(PDO::FETCH_COLUMN);
+if (!$ploshadka_id)exit;
+
 $redis = new Redis();
 $redis->pconnect('185.75.90.54', 6379);
 $redis->select(1);
 $arr=explode(',',addslashes($_GET['anons_ids']));
+
 foreach ($arr as $value) {
     $value=substr($value, 0, -1);
     $redis->incr(date('d').':'.$value);
 };
 
-$domen=parse_url ( $_SERVER['HTTP_ORIGIN'], PHP_URL_HOST );
-
-require_once('/var/www/www-root/data/www/panel.cortonlab.com/config/db.php');$sql= "SELECT `id` FROM `ploshadki` WHERE `domen`='".$domen."'";
-$ploshadka_id = $GLOBALS['db']->query($sql)->fetch(PDO::FETCH_COLUMN);
-
+$valueold="";
 $redis->select(3);
 foreach ($arr as $value) {
     $value=substr($value, -1);
     $redis->incr(date('d').':'.$ploshadka_id.':'.$value);
-};
+    if ($value!=$valueold){
+        $sql= "INSERT INTO `widget_prosmotr`(`prosmotr_id`, `widget`, `ploshadka_id`, `date`) VALUES ('".addslashes($_GET['prosmort_id'])."','".$value."','".$ploshadka_id."',CURDATE())";
+        $GLOBALS['dbstat']->query($sql);
+        $valueold=$value;
+    }
 
+};
 $redis->close();
