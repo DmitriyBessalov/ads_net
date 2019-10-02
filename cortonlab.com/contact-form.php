935748fea6b5655e752f324ae2d4f3c3decf9583
dmitriy <dmitriy@bessalov.ru>
2019-10-02 18:34:21 +0300

    -

--- cortonlab.com/contact-form.php
+++ cortonlab.com/contact-form.php
@@ -2,15 +2,19 @@
 $_POST = array_map('addslashes', $_POST);
 
 if(!empty($_POST['email'] )) {
-    $to = "info@cortonlab.com";
+    $to = "dmitriy@bessalov.ru";
     $subject = "Corton лендинг форма";
     $message = '
          <h3>Получен запрос на добавление '.$_POST['type'].'</h3></br>
          <b>Еmail: </b>'.$_POST['email'].'</br>
          <b>IP: </b>'.$_SERVER['REMOTE_ADDR'].'</br>';
-    $headers  = "Content-type: text/html; charset=UTF-8 \r\nFrom: <info@cortonlab.com>\r\n";
-    $result = mail($to, $subject, $message, $headers);
-    echo "<p class=\"textred\" style='color: #008a00'>Запрос отправлен. Спасибо.</p>";
+    $headers  = "Content-type: text/html; charset=UTF-8 \r\nFrom: <dmitriy@bessalov.ru>\r\n";
+    $result = mail($to, $subject, $message);
+    if (!$result){
+        echo "<p class=\"textred\">Системная ошибка</p>";
+    }else{
+        echo "<p class=\"textred\" style='color: #008a00'>Запрос отправлен. Спасибо.</p>";
+    }
 }else{
     echo "<p class=\"textred\">Email не введён</p>";
 }
