fcf81820961524280d5906f434e47952e12756db
dmitriy <dmitriy@bessalov.ru>
2019-10-02 18:39:38 +0300

    -

--- cortonlab.com/contact-form.php
+++ cortonlab.com/contact-form.php
@@ -5,7 +5,7 @@ if(!empty($_POST['email'] )) {
     $to = "info@cortonlab.com";
     $subject = "Corton лендинг форма";
     $message = '
-         <h3>Получен запрос на добавление '.$_POST['type'].'</h3></br>
+         <h3>Получен запрос на добавление '.$_POST['type'].'</h3><br>
          <b>Еmail: </b>'.$_POST['email'].'<br>
          <b>IP: </b>'.$_SERVER['REMOTE_ADDR'].'<br>';
     $headers  = "Content-type: text/html; charset=UTF-8 \r\nFrom: <info@cortonlab.com>\r\n";
