b1ef35f97284ff0807cce0d1ec5bf22b0fbe18f8
dmitriy <dmitriy@bessalov.ru>
2019-10-28 13:12:18 +0300

    -

--- stat.cortonlab.com/widget_show.php
+++ stat.cortonlab.com/widget_show.php
@@ -37,10 +37,21 @@ $valueold="";
 
 $stat_arr['recomend']=(int)0;
 
+$platform['CPM_stavka']=$platform['CPM_stavka']/1000;
+
+function cpm($platform){
+    $sql = "UPDATE `balans_ploshadki` SET `cpm`= `cpm` + " . $platform['CPM_stavka'] . "  WHERE `date`=CURDATE() AND `ploshadka_id`='" . $platform['id'] . "'";
+    if (!$GLOBALS['dbstat']->exec($sql)) {
+        $sql = "INSERT INTO `balans_ploshadki` SET `ploshadka_id` = '" . $platform['id'] . "', `date` = CURDATE(), `cpm`=  '" . $platform['CPM_stavka'] . "'";
+        $GLOBALS['dbstat']->query($sql);
+    }
+};
+
 foreach ($arr as $value) {
     $value=substr($value, -1);
     if ($value=='e'){
         $stat_arr['native']=1;
+        cpm($platform);
     }
     if ($value=='r'){
         $stat_arr['recomend']++;
@@ -56,7 +67,6 @@ foreach ($arr as $value) {
 $redis->close();
 
 # Модель оплаты за показы виджетов
-$platform['CPM_stavka']=$platform['CPM_stavka']/1000;
 if($platform['model_pay']=='CPM') {
     $sql = "UPDATE `balans_user` SET `balans` = `balans` + " . $platform['CPM_stavka'] . ", `CPM`= `CPM` + " . $platform['CPM_stavka'] . "  WHERE `date`=CURDATE() AND `user_id`='" . $platform['user_id'] . "'";
 }else{
@@ -73,10 +83,8 @@ if (!$GLOBALS['db']->exec($sql)) {
     $GLOBALS['db']->query($sql);
 }
 
-$sql = "UPDATE `balans_ploshadki` SET `cpm`= `cpm` + " . $platform['CPM_stavka'] . "  WHERE `date`=CURDATE() AND `ploshadka_id`='" . $platform['id'] . "'";
-if (!$GLOBALS['dbstat']->exec($sql)) {
-    $sql = "INSERT INTO `balans_ploshadki` SET `ploshadka_id` = '" . $platform['id'] . "', `date` = CURDATE(), `cpm`=  '".$platform['CPM_stavka']."'";
-    $GLOBALS['dbstat']->query($sql);
+if ($_GET['f']==1){
+    cpm($platform);
 }
 
 statpostgres($stat_arr);
